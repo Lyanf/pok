8e301c100a240ab995d540b8117618ae10f2ed67
ubuntu <ubuntu@localhost.localdomain>
2019-12-07 19:04:01 +0800

    good run

--- examples/semaphores/pr1/activity.c
+++ examples/semaphores/pr1/activity.c
@@ -22,39 +22,51 @@
 #include <types.h>
 extern uint8_t sid;
 uint8_t val;
-void cpu_work(int cycle) {
+
+void printu64(uint64_t x) {
+    char* p = &x;
+    for(int i = 0; i < 8; ++i) {
+    	printf("%x", p[i]);
+    }
+
+}
+void cpu_work(int cycle,int id) {
 	printf("################# test #################\n");
+	uint64_t q = 1;
 	pok_time_t start, end;
 	pok_time_get(&start);
+	printf("start-time:%u\n",start);
 	for (int i = 0; i < cycle; i++) {
-//		for (int j = 2; j < 4; j++) {
-//			for (int k = 2; k < j; k++) {
-//				int temp = j % k;
-//			}
-//
-		//}
-		pok_thread_sleep(2000000);
+		for (int j = 2; j < 1000; j++) {
+			for (int k = 2; k < j; k++) {
+				int temp = j % k;
+			}
+
+		}
+		//pok_thread_sleep(2000000);
 	}
 	pok_time_get(&end);
-	printf("time: %lu\n", end-start);
+	printf("end-time:%u\n",end);
+	printf("time----%d: %u\n",id, end-start);
+//	printu64(end-start);
 }
 void* pinger_job()
 {
 	printf("thead 1");
-	cpu_work(10);
-	while(1);
+	cpu_work(1000,1);
+//	while(1);
 }
 
 void* pinger_job2()
 {
 	printf("thread 2");
-	cpu_work(20);
-	while(1);
+	cpu_work(100,2);
+//	while(1);
 }
 
 void* pinger_job3()
 {
 	printf("thread 3");
-	cpu_work(5);
-	while(1);
+	cpu_work(500,3);
+//	while(1);
 }
